5432a888c9c1c0d6b9a09d43007e6c4585693211
hannissey <hannahvclarke@gmail.com>
2021-12-16 11:58:57 +0000

    self test questions complete

--- src/chapter1_theFirstStep/helloWorld.java
+++ src/chapter1_theFirstStep/helloWorld.java
@@ -1,7 +1,45 @@
 package chapter1_theFirstStep;
 
-public class helloWorld {
-	public static void main (String[] args) {
-		System.out.println("Hello World!");
+// header
+public class helloWorld 
+{ 
+	public static void main (String[] args) 
+	{
+		// display Hello World on the screen
+		System.out.print("Hello World!");
+		// every java instruction ends with a semi-colon
+		System.out.println("Hello World again!");
+		// print does not have line breaks
+		System.out.print("This will be on ");
+		System.out.println("the same line. (line break here)");
+		//System.out.println();
+		System.out.println("This will not (line break here)");
+		System.out.print("be on the same line.");
+		
+		System.out.print("Hello, how are you? ");
+		System.out.println("Fine thanks.");
+		
+		System.out.println("1 + 2 " + "+ 3" + " = 6");
 	}
 }
+
+/* keywords
+ * 
+ * public
+ * 	makes our class accessible to other classes
+ * 
+ * class
+ *	Object oriented languages require the program to be written
+ *  in separate units called classes
+ *
+ * main
+ * 	special method which contains the program and runs it from
+ *	start to finish 
+ * 
+ * "Hello World!"
+ *  literal string
+ * 
+ * 
+ * 
+  
+ */
