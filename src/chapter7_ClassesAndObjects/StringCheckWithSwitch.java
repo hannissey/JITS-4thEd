65235f46a10c2183726cfa71d6128d9c0ce9a8e0
hannissey <hannahvclarke@gmail.com>
2022-01-14 15:35:29 +0000

    stopping for the day

---
+++ src/chapter7_ClassesAndObjects/StringCheckWithSwitch.java
@@ -0,0 +1,24 @@
+package chapter7_ClassesAndObjects;
+
+import java.util.Scanner;
+
+public class StringCheckWithSwitch {
+	public static void main(String[] args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+		String symbol;
+		
+			// get symbol from user
+			System.out.println("Enter the symbol (square/circle/triangle):");
+			symbol = keyboard.next();
+			// use string object in switch statement
+			switch (symbol)
+			{
+			case "square" : System.out.println("ATTACK"); break;
+			case "circle" : System.out.println("BLOCK"); break;
+			case "triangle" : System.out.println("JUMP"); break;
+			default : System.out.println("Invalid choice");
+			}			
+
+	}
+}
