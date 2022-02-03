7d08b3cae37dcb6f3b36537e298fc91287af3b71
hannissey <hannahvclarke@gmail.com>
2022-02-03 11:07:18 +0000

    started chapter 10, installing javafx

---
+++ src/chapter10_IntroducingJavaFX/SmileyFace.java
@@ -0,0 +1,17 @@
+package chapter10_IntroducingJavaFX;
+
+//import javafx.application.Application; 
+//import javafx.scene.Group; 
+//import javafx.scene.Scene; 
+//import javafx.scene.paint.Color; 
+//import javafx.scene.shape.Arc; 
+//import javafx.scene.shape.ArcType; 
+//import javafx.scene.shape.Circle; 
+//import javafx.scene.text.Font; 
+//import javafx.scene.text.Text; 
+//import javafx.stage.Stage; 
+
+
+public class SmileyFace {
+
+}
